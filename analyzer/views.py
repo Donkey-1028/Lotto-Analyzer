cfb32ef2392d0dba2bf65397d935746670a5253b
Donkey-1028 <anbo10283182@gmail.com>
2020-04-17 22:13:41 +0900

    home page 프로토 타입 추가

--- analyzer/views.py
+++ analyzer/views.py
@@ -6,20 +6,27 @@ from django.views.generic.detail import DetailView
 from .models import LottoCount
 
 
+def home(request):
+    lotto = LottoCount.objects.filter(id=4).values()[0]  # 전체보기로 지정된 object
+    lotto_count = [value for _, value in enumerate(lotto.values())]
+    lotto_number = [value for _, value in enumerate(lotto.keys())]
+    return render(request, 'home.html', {'lotto': zip(lotto_number, lotto_count)})
+
+
 class UpdateNewLotto(View):
     def get(self, request, *args, **kwargs):
         LottoCount.objects.update_new_lotto(2, 1)
-        LottoCount.objects.create_many_lotto_count(10)
 
         return render(request, 'test.html')  # 하드코딩 되어 있음
 
 
 class CreateManyLottoCount(View):
     def get(self, request, *args, **kwargs):
-        LottoCount.objects.create_many_lotto_count(906)
+        LottoCount.objects.create_many_lotto_count(1)
         return redirect('/')  # render 할 경우 반환시간 만료 발생. 수정이 필요해 보인다
 
 
 class ShowLottoCount(DetailView):
     model = LottoCount
     template_name = 'index.html'
+
