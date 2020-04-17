7aa93c9b3a7d9150057255236ea82dd77bed4fa5
Donkey-1028 <anbo10283182@gmail.com>
2020-04-17 22:12:49 +0900

    메소드 staff_member_required 권한 적용

--- analyzer/admin.py
+++ analyzer/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.shortcuts import redirect, render
 from django.urls import path
 from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 
 from .models import LottoCount
 
 
 class LottoCountManager(admin.ModelAdmin):
     list_display = ['id', 'first_drwNo', 'final_drwNo']
+    change_list_template = 'admin/change_list_template.html'
 
     def get_urls(self):
         urls = super().get_urls()
@@ -17,16 +19,17 @@ class LottoCountManager(admin.ModelAdmin):
         ]
         return my_urls + urls
 
-    @staff_member_required(login_url='admin:login')
+    @method_decorator(staff_member_required)
     def create_index(self, request):
         return render(request, 'admin/create.html')
 
-    @staff_member_required(login_url='admin:login')
+    @method_decorator(staff_member_required)
     def create_lotto(self, request):
         first_drwNos = request.GET.get('first_drwNo')
         final_drwNos = request.GET.get('final_drwNo')
         LottoCount.objects.create_many_lotto_count(int(final_drwNos), int(first_drwNos))
         return redirect('admin:analyzer_lottocount_changelist')
+
 
 admin.site.register(LottoCount, LottoCountManager)
 # Register your models here.
