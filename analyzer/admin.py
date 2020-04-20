23e3f322045a473c80b19b6340bf9cb700c7bd47
Donkey-1028 <anbo10283182@gmail.com>
2020-04-21 01:38:01 +0900

    최신 회차 업데이트 액션 추가

--- analyzer/admin.py
+++ analyzer/admin.py
@@ -6,10 +6,24 @@ from django.utils.decorators import method_decorator
 
 from .models import LottoCount
 
+DRWNO = 907  # 최신 회차 번호. 자동적으로 해당 번호를 만들어 올 수 있게 crontab 구현이 필요해보임
 
-class LottoCountManager(admin.ModelAdmin):
+
+def update_new_lotto_action(modeladmin, request, queryset):
+    """ 최신 Lotto 번호 update action"""
+    for query in queryset:
+        LottoCount.objects.update_new_lotto(query.id, DRWNO)
+        model = LottoCount.objects.get(id=query.id)
+        model.update_first_and_final()
+
+
+update_new_lotto_action.short_description = 'Update No.%d Lotto Count' % DRWNO
+
+
+class LottoCountAdminManager(admin.ModelAdmin):
     list_display = ['id', 'first_drwNo', 'final_drwNo']
     change_list_template = 'admin/change_list_template.html'
+    actions = [update_new_lotto_action]
 
     def get_urls(self):
         urls = super().get_urls()
@@ -31,5 +45,5 @@ class LottoCountManager(admin.ModelAdmin):
         return redirect('admin:analyzer_lottocount_changelist')
 
 
-admin.site.register(LottoCount, LottoCountManager)
+admin.site.register(LottoCount, LottoCountAdminManager)
 # Register your models here.
