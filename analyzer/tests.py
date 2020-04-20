79d9998318430361f532f079d4057fb2e454c6cb
Donkey-1028 <anbo10283182@gmail.com>
2020-04-20 17:29:54 +0900

    LottoCountAdminManager 테스트 케이스 추가

--- analyzer/tests.py
+++ analyzer/tests.py
@@ -1,9 +1,14 @@
 import requests
 
 from django.test import TestCase
+from django.contrib.admin.sites import AdminSite
+from django.contrib.admin.options import ModelAdmin
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 from .models import LottoCount, COUNT_WORD_DICTIONARY
 from .lotto import get_lotto_number, get_all_lotto_number_count
+from .admin import LottoCountAdminManager
 
 
 class LottoAPITest(TestCase):
@@ -115,3 +120,54 @@ class LottoCountTest(TestCase):
         model = LottoCount.objects.create()
         with self.assertRaisesMessage(ValueError, 'update 할 일자가 없습니다.'):
             model.update_first_and_final()
+
+
+class MockRequest:
+    """ Request를 위한 HttpRequest 모조 클래스"""
+    def __init__(self, user=None):
+        self.user = user
+
+
+class LottoCountAdminManagerTest(TestCase):
+    """ LottoCountAdminManager 테스트"""
+    def setUp(self):
+        self.super_user = User.objects.create_superuser(username='test_admin', password='testadminpassword')
+        self.user = User.objects.create_user(username='test_user', password='testpassword')
+        self.request = MockRequest()
+        self.model_admin = LottoCountAdminManager(model=LottoCount, admin_site=AdminSite())
+
+    def test_create_index_like_doc(self):
+        """ 공식문서처럼 URL 을 이용한 테스트가 아닌 AdminManager를 통해서 테스트.
+        관리자 계정으로 request할 경우 create_index 뷰가 정상적인 상태코드 반환"""
+        self.request.user = self.super_user
+        response = self.model_admin.create_index(request=self.request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_index(self):
+        """ 일반적인 URL을 이용한 AdminManager 테스트."""
+        url = reverse('admin:index')
+        self.client.login(username='test_admin', password='testadminpassword')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_index_without_staff_permission_like_doc(self):
+        """ staff 가 아닌 유저가 create_index 뷰에 접근하려고 할 때 테스트.
+        예상된 response_code는 권한 없음이지만 아예 response_code가 반환하기 전에 에러가 발생한다.
+        예상되는 이유로는 데코레이터에 의해 staff 권한이 없을 경우 자동으로 admin:login으로 redirect 되는데
+        이때 HttpRequest의 build_absolute_uri 메소드를 이용해서 해당 URL이 지정된다. 하지만 앞서 정의한
+        모조품 Request인 MockReuqest 클래스는 해당 메소드가 없기 때문에 build_absolute_uri 메소드가 없다고
+        AttributeError 가 발생한다. """
+        self.request.user = self.user
+        with self.assertRaisesMessage(AttributeError, ''):
+            self.model_admin.create_index(request=self.request)
+
+    def test_create_index_without_staff_permission(self):
+        """ 위의 테스트와 마찬가지로 권한 없다는 status code인 401으로 예상을 했지만
+        staff_member_required 데코레이터에 의해 redirect 되기 때문에 302 코드 반환."""
+        url = reverse('admin:create_index')
+        self.client.login(username='test_user', password='testpassword')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 302)
